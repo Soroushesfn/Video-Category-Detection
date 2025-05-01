07f4dc3a90e905c2187b241f142a2ef6f37c5690
Soroushesfn <Soroush.esfahanian83@gmail.com>
2025-05-01 20:19:40 +0330

    added tags and title embeddings

--- scripts/feature_engineering.py
+++ scripts/feature_engineering.py
@@ -2,9 +2,4 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 
 df = pd.read_csv('database/Data.csv', encoding='ISO-8859-1')
-sample_channel = df['channel_title'][0]
-print(sample_channel)
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-title_embeddings = df['title'].fillna('').apply(model.encode)
-print(title_embeddings)
+print(df['categoryID'].unique())
