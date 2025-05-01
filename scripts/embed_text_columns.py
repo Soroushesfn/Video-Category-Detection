b5ee39f0028faf2259e1c9dcebcec9be2b213ac1
Soroushesfn <Soroush.esfahanian83@gmail.com>
2025-05-01 20:09:47 +0330

    added the embedding script

---
+++ scripts/embed_text_columns.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import os
+from sentence_transformers import SentenceTransformer
+import pickle
+
+# Meta data
+# Columns
+TEXTUAL_COLUMNS = ["title", "tags", "description"]
+# Embedding model
+MODEL_NAME = "all-MiniLM-L6-v2"
+OUTPUT_DIR = "tmp/embeddings/"
+
+# Load Data
+print("[EMBEDDING][INFO]: Loading data...")
+df = pd.read_pickle('tmp/raw_data.pkl')
+
+# Load embedding model
+print(f"[EMBEDDING][INFO]: Loading embedding model: {MODEL_NAME}...")
+model = SentenceTransformer(MODEL_NAME)
+
+# Clean tags
+def clean_tags(text):
+    return " ".join(tag.replace('"', '') for tag in str(text).split('|'))
+
+# Embedding each column
+for column in TEXTUAL_COLUMNS:
+    print(f"[EMBEDDING][INFO]: Embedding column: {column}...")
+    
+    if column == "tags":
+        texts = df[column].fillna("").apply(clean_tags).tolist()
+    else:
+        texts = df[column].fillna("").astype(str).tolist()
+    
+    # perform embedding
+    embeddings = model.encode(texts, show_progress_bar=True, batch_size=32)
+    
+    # Save
+    out_path = OUTPUT_DIR + f"{column}_embeddings.npy"
+    np.save(out_path, embeddings)
+    print(f"[EMBEDDING][SUCCESS]: Saved embeddings to: {out_path}")
--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,11 +1,10 @@
-
 import sqlite3
 import pandas as pd
 import os
 from database_connection import get_db_connection
 
+TABLE_NAME = 'US_Trending_Videos'
 db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "database", "dataset.db"))
-
 conn = get_db_connection(db_path)
 
 if conn:
@@ -13,14 +12,13 @@ if conn:
 else:
     print("Database connection failed.")
 
-
-
 dataframes = {}
 for table_name in tables['name']:
     df = pd.read_sql(f"SELECT * FROM {table_name}", conn)
     dataframes[table_name] = df
 
+# Save as pickle
+dataframes['US_Trending_Videos'].to_pickle('tmp/raw_data.pkl')
 
 if conn:
     conn.close()
-
