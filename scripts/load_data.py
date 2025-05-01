aa8a12811193d4c8267efb51415cd0eec642af90
ali zm <ali.zm.1383@gmail.com>
2025-05-01 10:50:41 +0330

    Added load_data script

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,1 +1,19 @@
 
+import sqlite3
+import pandas as pd
+import os
+
+db_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "database", "dataset.db"))
+conn = sqlite3.connect(db_path)
+
+
+query = "SELECT name FROM sqlite_master WHERE type='table';"
+tables = pd.read_sql(query, conn)
+
+dataframes = {}
+for table_name in tables['name']:
+    df = pd.read_sql(f"SELECT * FROM {table_name}", conn)
+    dataframes[table_name] = df
+
+
+conn.close()
