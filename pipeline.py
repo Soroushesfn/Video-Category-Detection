3f49e0153507bd87f8f55a70a2b225c7a8918b81
ali zm <ali.zm.1383@gmail.com>
2025-06-05 19:25:33 +0330

    Added training and prediction into pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,24 @@
 import subprocess
+import argparse
 
-subprocess.run(["python", "scripts/load_data.py"])
-subprocess.run(["python", "scripts/feature_engineering.py"])
-subprocess.run(["python", "scripts/preprocess.py"])
+parser = argparse.ArgumentParser(description="Run data pipeline")
+parser.add_argument(
+    "--mode",
+    type=str,
+    choices=["train", "test"],
+    help="Mode to run the pipeline: 'train' or 'test'"
+)
+args = parser.parse_args()
+
+if args.mode == "train":
+    subprocess.run(["python", "scripts/load_data.py"])
+    subprocess.run(["python", "scripts/feature_engineering.py"])
+    subprocess.run(["python", "scripts/preprocess.py"])
+    subprocess.run(["python", "scripts/splitData.py"])
+    subprocess.run(["python", "scripts/train_model.py"])
+
+elif args.mode == "test":
+    subprocess.run(["python", "scripts/make_prediction.py"])
+
+else:
+    print("No valid mode provided — doing nothing.")
