bbace1bf28efcb96965e66890e757b614d44075f
ali zm <ali.zm.1383@gmail.com>
2025-06-06 12:15:58 +0330

    Remove test from train

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -103,25 +103,3 @@ history = model.fit(
 )
 
 saving.save_model(model, tmp_path + '/3branchMlp_9157.keras')
-
-X_test = pd.read_pickle(tmp_path+"/X_test.pkl")
-y_test = pd.read_pickle(tmp_path+"/y_test.pkl")
-X_title_test = X_test[[col for col in X_test.columns if col.startswith('title_emb')]]
-X_tags_test = X_test[[col for col in X_test.columns if col.startswith('tags_emb')]]
-X_desc_test = X_test[[col for col in X_test.columns if col.startswith('desc_emb')]]
-X_numeric_test = X_test[['views', 'comment_count', 'engagement_rate', 'like_dislike_ratio', 'tag_count']]
-
-
-y_pred = model.predict({
-    'title_embedding': X_title_test,
-    'tags_embedding': X_tags_test,
-    'description_embedding': X_desc_test,
-    'numeric_features': X_numeric_test
-})
-
-
-y_true_classes = y_test.to_numpy().argmax(axis=1)
-y_pred_classes = y_pred.argmax(axis=1)
-y_pred_df = pd.DataFrame({'prediction': y_pred_classes})
-y_pred_df.to_csv(tmp_path+ "/prediction.csv")
-print(classification_report(y_true_classes, y_pred_classes, digits=4))
